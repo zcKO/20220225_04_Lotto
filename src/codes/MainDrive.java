ededc274443e74cb25e7eda2bb365ba4ec710839
zcKO <studyJcHub@gmail.com>
2022-02-25 12:21:51 +0900

    입력한 숫자가 검사에 통과하면 배열에 집어넣기
    - 중복 검사도 같이 수행

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -9,6 +9,8 @@ public class MainDrive {
 //        6개의 숫자 입력 받기
         Scanner myScanner = new Scanner(System.in);
 
+        int[] myNumbers = new int[6];
+
         for (int i = 0; i < 6; i++) {
 
             while (true) {
@@ -17,11 +19,20 @@ public class MainDrive {
                 int inputNum = myScanner.nextInt();
 
                 boolean isRangeOk = (1 <= inputNum) && (inputNum <= 45);
+                boolean isDuplOk = true;
 
-                if (isRangeOk) {
+                for (int myNum : myNumbers) {
+                    if (myNum == inputNum) {
+                        isDuplOk = false;
+                        break;
+                    }
+                }
+
+                if (isRangeOk && isDuplOk) {
+                    myNumbers[i] = inputNum;
                     break;
                 } else {
-                    System.out.println("범위가 벗어 났습니다. 다시 입력해주세요");
+                    System.out.println("동일한 숫자 또는 범위 (1 ~ 45)가 벗어 났습니다. 다시 입력해주세요");
                 }
 
             }
